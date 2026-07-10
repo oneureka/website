a6ff1907085b32403dd4e6a02aca5b530e69ab20
oneureka <65535758+oneureka@users.noreply.github.com>
2026-07-10 23:48:13 +0800

    refactor: restructure src/views to src/pages with kebab-case naming

--- hanaland/src/router/index.ts
+++ hanaland/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TopicList from '@views/TopicList.vue'
+import TopicList from '@pages/topics.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,53 +9,53 @@ const router = createRouter({
     {
       path: '/topics/new',
       name: 'topic-new',
-      component: () => import('@views/TopicNew.vue'),
+      component: () => import('@pages/topics/new.vue'),
     },
     {
       path: '/topics/:id',
       name: 'topic-detail',
-      component: () => import('@views/TopicDetail.vue'),
+      component: () => import('@pages/topics/[id].vue'),
     },
-    { path: '/login', name: 'login', component: () => import('@views/Login.vue') },
+    { path: '/login', name: 'login', component: () => import('@pages/login.vue') },
     {
       path: '/explore',
       name: 'explore',
-      component: () => import('@views/Explore.vue'),
+      component: () => import('@pages/explore.vue'),
     },
     {
       path: '/notifications',
       name: 'notifications',
-      component: () => import('@views/Notifications.vue'),
+      component: () => import('@pages/notifications.vue'),
     },
     {
       path: '/top100',
       name: 'top100',
-      component: () => import('@views/Top100.vue'),
+      component: () => import('@pages/top100.vue'),
     },
     {
       path: '/:login',
-      component: () => import('@views/UserHome.vue'),
+      component: () => import('@pages/[login].vue'),
     },
     {
       path: '/:login/replies',
-      component: () => import('@views/UserReplies.vue'),
+      component: () => import('@pages/[login]/replies.vue'),
     },
     {
       path: '/:login/followers',
-      component: () => import('@views/UserFollowers.vue'),
+      component: () => import('@pages/[login]/followers.vue'),
     },
     {
       path: '/:login/following',
-      component: () => import('@views/UserFollowing.vue'),
+      component: () => import('@pages/[login]/following.vue'),
     },
     {
       path: '/:login/favorites',
-      component: () => import('@views/UserFavorites.vue'),
+      component: () => import('@pages/[login]/favorites.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
-      component: () => import('@views/NotFound.vue'),
+      component: () => import('@pages/[...pathMatch].vue'),
     },
   ],
 })
